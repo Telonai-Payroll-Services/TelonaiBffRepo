7cdb733e5fb4519354a3e498854f9464f31e508c
aktelonai <aklilemariam.alemu@telonai.com>
2024-11-09 12:59:55 +0300

    update signpdf service

--- Telonai/TelonaiBFF/Services/DocumentService.cs
+++ Telonai/TelonaiBFF/Services/DocumentService.cs
@@ -12,8 +12,6 @@ using System.Text;
 using Microsoft.OpenApi.Extensions;
 using Document = Entities.Document;
 
-//using Amazon.Runtime.Documents;
-
 public interface IDocumentService
 {
     Task<DocumentModel> GetOwnDocumentDetailsByDocumentTypeAsync(DocumentTypeModel documentType);
@@ -448,10 +446,10 @@ public class DocumentService : IDocumentService
     }
     public async Task<byte[]> SignW4DoumentAsync(Guid id, int employmentId, SignatureModel signature)
     {
-        var emp = _context.Employment.Find(employmentId);
+        var emp = _context.Employment.Include(e => e.Person).FirstOrDefault(e => e.Id == employmentId);
         _scopedAuthorization.ValidateByJobId(_httpContextAccessor.HttpContext.User, AuthorizationType.User, emp.JobId);
-
-        var documentType = DocumentTypeModel.WFourUnsigned;
+        _person = emp.Person;
+        var documentType = DocumentTypeModel.WFourUnsigned;      
         var document = await GetDocumentByDocumentTypeAndIdAsync(documentType, id);
         if (document == null)
         {
@@ -507,9 +505,9 @@ public class DocumentService : IDocumentService
             throw new InvalidOperationException("No filing status selected or more than one status selected.");
         }
 
-        var documentResult = await _documentManager.GetDocumentByTypeAndIdAsync(DocumentTypeModel.WFourUnsigned.ToString(),
-            document.Id.ToString());
-
+         var documentResult = await _documentManager.GetDocumentByTypeAndIdAsync(DocumentTypeModel.WFourUnsigned.ToString(),
+             document.Id.ToString());
+        
         var fileBytes = await SetPdfFormFilds(model, documentResult, filingStatus.Item1);
 
         var doumentId = await SaveGeneratedUnsignedW4Pdf(document.FileName, fileBytes);
@@ -534,13 +532,12 @@ public class DocumentService : IDocumentService
 
     public async Task<Guid> UpdateW4PdfWithSignature(Guid id, byte[] file)
     {
-        var person = await _personService.GetCurrentUserAsync(); ;
 
         var documentModel = new DocumentModel
         {
-            FileName = person.FirstName + person.MiddleName + person.LastName + DocumentTypeModel.WFour.GetDisplayName(),
+            FileName = _person.FirstName + _person.MiddleName + _person.LastName + DocumentTypeModel.WFour.GetDisplayName(),
             DocumentType = DocumentTypeModel.WFour,
-            PersonId = person.Id
+            PersonId =  _person.Id
         };
         using Stream stream = new MemoryStream(file);
 
@@ -598,6 +595,5 @@ public class DocumentService : IDocumentService
         var documentModel=EmployeeWithholdingHelper.CreateDocumentModel(documentId, filename, _person.Id, effectiveDate);
         return documentModel;
     }
-    //private  Person GetPerson( int id) { return  _context.Person.Find(id); }
 }
 
