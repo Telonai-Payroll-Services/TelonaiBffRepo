7ae6fae7f9b2710798dfcb100484b1ed5b6b019b
meronkassa12 <152719859+meronkassa12@users.noreply.github.com>
2024-11-17 21:28:09 -0500

    Changes AgentId field to AgentCode

--- Telonai/TelonaiBFF/Models/EmployerInvitationModel.cs
+++ Telonai/TelonaiBFF/Models/EmployerInvitationModel.cs
@@ -1,8 +1,12 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
 namespace TelonaiWebApi.Models;
 
 
 public class EmployerInvitationModel : BaseTracker
-{   public string Email { get; set; }
+{   
+    public int Id { get; set; }
+    public string Email { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string TaxId { get; set; }
@@ -15,5 +19,6 @@ public class EmployerInvitationModel : BaseTracker
     public ulong RoutingNumber { get; set; }
     public ulong AccountNumber { get; set; }
     public ulong AccountNumber2 { get; set; }
+    public ushort AgentCode { get; set; }
 
 }
