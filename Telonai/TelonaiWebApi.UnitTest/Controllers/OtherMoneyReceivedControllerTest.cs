315f88cebc8e60d0e5a0b71c4d79cd6f93446b59
biruk-telonai <biruk.wolde@telonai.com>
2024-11-28 15:36:19 +0300

    TPS-117 controller test

---
+++ Telonai/TelonaiWebApi.UnitTest/Controllers/OtherMoneyReceivedControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelonaiWebApi.Controllers;
+using TelonaiWebApi.Helpers;
+using TelonaiWebApi.Services;
+
+namespace TelonaiWebAPI.UnitTest.Controllers
+{
+    public class OtherMoneyReceivedControllerTest
+    {
+        private readonly Mock<IScopedAuthorization> _mockScopedAuthorization;
+        private readonly Mock<IOtherMoneyReceivedService> _mockOtherMoneyReceivedService;
+        private readonly Mock<OtherIncomeController> _otherMoneyReceivedController;
+        public OtherMoneyReceivedControllerTest()
+        {
+          _mockScopedAuthorization = new  Mock<IScopedAuthorization>();
+          _mockOtherMoneyReceivedService = new Mock<IOtherMoneyReceivedService>();
+          _otherMoneyReceivedController = new OtherIncomeController(_mockOtherMoneyReceivedService.Object, _mockScopedAuthorization.Object);
+        }
+    }
+}
