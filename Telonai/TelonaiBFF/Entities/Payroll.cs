6164c346b8f5a8c75ee5430bc5ab4ce31f7e1cd4
meronkassa12 <152719859+meronkassa12@users.noreply.github.com>
2024-11-28 19:21:51 -0500

    Fixed creation of stubs and expense tracker

--- Telonai/TelonaiBFF/Entities/Payroll.cs
+++ Telonai/TelonaiBFF/Entities/Payroll.cs
@@ -7,9 +7,8 @@ public class Payroll: BaseTracker
     public int Id { get; set; }
     public int CompanyId { get; set; }
     public int PayrollScheduleId { get; set; }
-
     public Company Company { get; set; }
-    public DateOnly ScheduledRunDate { get; set; }   //This is the last date of this payroll period
+    public DateOnly ScheduledRunDate { get; set; }   //This is normally the last date of this payroll period unless affected by weekend or holiday
     public DateOnly StartDate { get; set; }
     public DateTime? TrueRunDate { get; set; }
     public PayrollSchedule PayrollSchedule { get; set; }
@@ -19,5 +18,6 @@ public class Payroll: BaseTracker
     public double? EmployeesPaid { get; set; }
     public double? StatesPaid { get; set; }
     public double? FederalPaid { get; set; }
+    public double? GrossPay { get; set; }
 
 }
