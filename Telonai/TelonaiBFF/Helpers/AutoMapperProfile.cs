ebe42626366d9dc111636da93ccc32be608b50dc
Surafel Teklegiorgis <surafel.teklegiorgis@telonai.com>
2024-12-21 19:08:44 +0300

    Invitation status fixed

--- Telonai/TelonaiBFF/Helpers/AutoMapperProfile.cs
+++ Telonai/TelonaiBFF/Helpers/AutoMapperProfile.cs
@@ -259,7 +259,7 @@ public class AutoMapperProfile : Profile
 
         CreateMap<Invitation, InvitationStatusModel>()
          .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FirstName + " " + src.LastName))
-         .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.UpdatedBy == null ? "Invitation Sent" : "Completed"));
+         .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.UpdatedDate == null ? "Invitation Sent" : "Completed"));
 
         CreateMap<Invitation, InvitationModel>()
             .ForMember(dest => dest.Employment, opt => opt.Ignore())
